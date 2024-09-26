3195143b84cc4a3490200c3c339ca6ef565d4f1d
jaqueslucas <lucassouza2347@hotmail.com>
2024-09-26 16:26:10 -0300

    alteracoes basicas feitas no projeto

--- src/components/NavButtons.tsx
+++ src/components/NavButtons.tsx
@@ -1,8 +1,13 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
 export default function NavButtons(){
+    const navigate = useNavigate();
+
     return(
         <div className="buttonCollections">
-            <button className="active">Home</button>
-            <button>Metas</button>
+            <button onClick={() => navigate('/')} className="active">Home</button>
+            <button onClick={() => navigate('/metas')}>Metas</button>
             <button>Badges</button>
         </div>
     )
